fcc09336d3996018db9b692176b642af15295dd0
AbdinatifM <197330086+AbdinatifMohamed@users.noreply.github.com>
2026-06-15 20:20:35 -0500

    Everything functionally is done, but we need to make it desktop responsive.

--- index.js
+++ index.js
@@ -10,7 +10,18 @@ const startscreen = document.getElementById("startscreen");
 const containerDiv = document.getElementById('textBoxWrapper');
 const divHeight = containerDiv.clientHeight; 
 const totalTextHeight = textBox.scrollHeight;
-
+const resultTitle = document.getElementById("resultTitle");
+const resultDesc = document.getElementById("resultDesc")
+const resultWPM = document.getElementById("resultWPM");
+const resultAccuracy = document.getElementById("resultAccuracy");
+const resultCharacters = document.getElementById("resultCharacters");
+const resultDiv = document.getElementById("resultDiv");
+const personalBest = document.getElementById("personalBest");
+const resultLogo = document.getElementById("resultLogo");
+const main = document.getElementById("main");
+const resultRepeat = document.getElementById("resultRepeat");
+const star = document.getElementById("star");
+const star2 = document.getElementById("star2");1
 
 let gameStarted = false;
 let time = 0;
@@ -21,6 +32,9 @@ let timeId;
 let currentString = null;
 let pointer  = 0;
 let startTime = Date.now();
+let result = 0;
+let WPM = 0;
+let firstGame = true;
 
 
 const createBackgroundTimer = () => {
@@ -30,8 +44,8 @@ const createBackgroundTimer = () => {
         if (time === 0) {
             endGame();
         }
-        currentWPM.innerText = updateWPM();
-
+        WPM = updateWPM();
+        currentWPM.innerText = WPM;
     }, 1000)
 }
 
@@ -47,6 +61,7 @@ const setupGame = async () => {
     pointer = 0;
     typed = 0;
     correctTyped = 0;
+    WPM = 0;
     try {
         const response = await fetch('./data.json');
 
@@ -72,6 +87,7 @@ const setupGame = async () => {
 
 const startGame = () => {
     gameStarted = true;
+    startTime = Date.now();
     document.addEventListener("keydown", onType);
     createBackgroundTimer();
 }
@@ -84,9 +100,54 @@ const onRestart = () => {
 const endGame = () => {
     clearInterval(timeId);
     document.removeEventListener("keydown", onType);
-    startTime = Date.now();
-    restartButton.classList.remove("hidden");
-    restartButton.addEventListener("click", onRestart);
+    main.classList.add("hidden");
+
+    if (firstGame) {
+        resultTitle.innerText = "Baseline Established!";
+        resultDesc.innerText = "You've set the bar. Now the real challenge begins~~time to beat it."
+        personalBest.innerHTML = `<span class="text-[#959597]">Best: </span>${WPM}WPM`;
+        resultLogo.classList.add("box-shadow");
+        resultLogo.src = "/assets/images/icon-completed.svg";
+        star.classList.remove("hidden");
+        star2.classList.remove("hidden");
+        result = WPM;
+    } else if (WPM < result) {
+        resultTitle.innerText = "Test Complete!";
+        resultDesc.innerText = "Solid run. Keep pushing to beat your score."
+        resultLogo.classList.add("box-shadow");
+        resultLogo.src = "/assets/images/icon-completed.svg";
+        star.classList.remove("hidden");
+        star2.classList.remove("hidden");
+    } else if (WPM > result) {
+        resultTitle.innerText = "High Score Smashed!";
+        resultDesc.innerText = "You're getting faster. That was incredible typing."
+        personalBest.innerHTML = `<span class="text-[#959597]">Best: </span>${WPM}WPM`;
+        resultLogo.classList.remove("box-shadow");
+        resultLogo.src = "./assets/images/icon-new-pb.svg";
+        star.classList.add("hidden");
+        star2.classList.add("hidden");
+        result = WPM;
+    }
+
+
+    resultWPM.innerText = WPM;
+    resultAccuracy.innerText = ((correctTyped/typed) * 100).toFixed(0) + "%"
+    resultCharacters.innerHTML = `<span class="text-[#4CD67A]">${correctTyped}<span/><span class="text-[#727279]">/<span/><span class="text-[#D64C5A]">${typed - correctTyped}<span/>`
+
+    resultDiv.classList.remove("hidden");
+    resultRepeat.addEventListener("click", () => {
+        main.classList.remove("hidden");
+        resultDiv.classList.add("hidden");
+        restartButton.classList.remove("hidden");
+        restartButton.addEventListener("click", onRestart);
+    }, { once: true })
+
+
+
+    if (firstGame){
+        firstGame = false;
+    }
+
 }
 
 
@@ -115,11 +176,14 @@ const onType = (e) => {
         }
     } else {
         endGame();
+        return;
     }
-    currentAccuracy.innerText =( (correctTyped/typed) * 100).toFixed(2) + "%"
+    currentAccuracy.innerText =( (correctTyped/typed) * 100).toFixed(0) + "%"
     typed++;
     spans[pointer].style.backgroundColor = "transparent";
     pointer++;
+
+    console.log("Pointer:", pointer, "spans length: ", spans.length);
     spans[pointer].style.backgroundColor = "rgb(149 149 151 / 40%)";
 }
 
@@ -132,7 +196,7 @@ function updateWPM() {
 
   const wpm = typed / timeInMinutes;
 
-  return wpm.toFixed(2);
+  return wpm.toFixed(0);
 }
 
 
